c7dbfe779deb777a44194f781b7a1b09450cf376
Luba <luciano.eq@outlook.com>
2022-07-25 04:45:58 -0300

    :sparkles: three more shorts.

---
+++ python/patter_matching.py
@@ -0,0 +1,7 @@
+command = input("What are you doing next? ")
+
+match command:
+    case "1":
+        print("Goodbye!")
+    case "2":
+        print("look")
---
+++ python/py_sets.py
@@ -0,0 +1,7 @@
+A = {1, 2, 3, 4, 5}
+B = {1, 3, 6, 7, 8}
+C = {10, 11}
+
+print(A.intersection(B, C))
+print(A.union(B, C))
+print(A.difference(B, C))
